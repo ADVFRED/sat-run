8b90a03d023196b44f3c10f722958683789898b0
ADVFRED <56049633+ADVFRED@users.noreply.github.com>
2023-01-10 20:16:51 -0600

    save point, prompt is buggy

--- main.py
+++ main.py
@@ -1,11 +1,68 @@
-def on_a_pressed():
-    if encounter != 0:
-        P1.set_position(P1.x, P1.y+1)
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+def on_up_pressed():
+    if encounter != 1:
+        P1.y += -20
+        P1.vy = 10
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+def on_on_overlap(sprite, otherSprite):
+    global encounter, WordIndice, InsertLocation, response, question
+    QuestionIndices: List[number] = []
+    WrongIndices: List[number] = []
+    # definitions
+    # , response
+    encounter = 1
+    WordIndice = randint(0, len(WordList))
+    InsertLocation = randint(0, 3)
+    response = 5
+    # make list of wrong answers
+    while len(WrongIndices) < 3:
+        indice = randint(0, len(WordList))
+        if WrongIndices.index(indice) < 0 and indice != WordIndice:
+            WrongIndices.append(indice)
+            QuestionIndices.append(indice)
+    # add right answer at random location
+    if InsertLocation != 3:
+        QuestionIndices.insert_at(0, WordIndice)
+    else:
+        QuestionIndices.append(WordIndice)
+        game.show_long_text(QuestionIndices.length, DialogLayout.BOTTOM)
+    question = "Definition of " + WordList[indice][0] + "is: [1] " + WordList[QuestionIndices[0]][1] + ", [2] " + WordList[QuestionIndices[1]][1] + ", [3] " + WordList[QuestionIndices[2]][1] + ", [4] " + WordList[QuestionIndices[3]][1]
+    #question = "num 1-4"
+    while encounter == 1:
+        game.show_long_text(question, DialogLayout.BOTTOM)
+        response = game.ask_for_number("answer (5 to show question again):")
+        if response == 5:
+            continue
+        else:
+            game.show_long_text("you answered!", DialogLayout.BOTTOM)
+            encounter = 0
+            P1.x += 40
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+question = ""
+response = 0
+InsertLocation = 0
+WordIndice = 0
 P1: Sprite = None
 encounter = 0
+WordList: List[List[str]] = []
+question22 = ""
+response23 = 0
+InsertLocation22 = 0
+WordIndice22 = 0
+response22 = 0
+WordIndice2 = 0
+InsertLocation2 = 0
+response2 = 0
+question2 = ""
 scene.set_background_color(6)
+WordList = [["abate", "to stop"],
+    ["abyss", "pit"],
+    ["exiate", "idk tbh"],
+    ["import", "bring in"]]
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
 encounter = 0
 P1 = sprites.create(img("""
         . . . . f f f f . . . . . 
@@ -26,5 +83,37 @@ P1 = sprites.create(img("""
             . . . f f . . f f . . . .
     """),
     SpriteKind.player)
-P1.set_position(0, 0)
-P1.set_velocity(50, 50)
+coin = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 5 5 5 . . . . 
+            . . . . 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+            . . . . 5 5 5 5 5 5 5 5 5 . . . 
+            . . . . . 5 5 5 5 5 5 5 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.food)
+coin.set_position(30, 105)
+P1.set_position(10, 100)
+P1.set_velocity(20, 60)
+scene.camera_follow_sprite(P1)
+
+def on_forever():
+    if P1.is_hitting_tile(CollisionDirection.RIGHT):
+        P1.vy = 0
+    else:
+        P1.vy = 10
+    if P1.is_hitting_tile(CollisionDirection.BOTTOM):
+        P1.vy = 0
+    else:
+        P1.vy = 10
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,31 +1,126 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (encounter != 0) {
-        P1.setPosition(P1.x, P1.y + 1)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    if (encounter != 1) {
+        P1.y += -20
+        P1.vy = 10
+    }
+    
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    let indice: number;
+    
+    let QuestionIndices : number[] = []
+    let WrongIndices : number[] = []
+    //  definitions
+    //  , response
+    encounter = 1
+    WordIndice = randint(0, WordList.length)
+    InsertLocation = randint(0, 3)
+    response = 5
+    //  make list of wrong answers
+    while (WrongIndices.length < 3) {
+        indice = randint(0, WordList.length)
+        if (WrongIndices.indexOf(indice) < 0 && indice != WordIndice) {
+            WrongIndices.push(indice)
+            QuestionIndices.push(indice)
+        }
+        
+    }
+    //  add right answer at random location
+    if (InsertLocation != 3) {
+        QuestionIndices.insertAt(0, WordIndice)
+    } else {
+        QuestionIndices.push(WordIndice)
+        game.showLongText(QuestionIndices.length, DialogLayout.Bottom)
+    }
+    
+    question = "Definition of " + WordList[indice][0] + "is: [1] " + WordList[QuestionIndices[0]][1] + ", [2] " + WordList[QuestionIndices[1]][1] + ", [3] " + WordList[QuestionIndices[2]][1] + ", [4] " + WordList[QuestionIndices[3]][1]
+    // question = "num 1-4"
+    while (encounter == 1) {
+        game.showLongText(question, DialogLayout.Bottom)
+        response = game.askForNumber("answer (5 to show question again):")
+        if (response == 5) {
+            continue
+        } else {
+            game.showLongText("you answered!", DialogLayout.Bottom)
+            encounter = 0
+            P1.x += 40
+        }
+        
     }
 })
-let P1: Sprite = null
+let question = ""
+let response = 0
+let InsertLocation = 0
+let WordIndice = 0
+let P1 : Sprite = null
 let encounter = 0
+let WordList : string[][] = []
+let question22 = ""
+let response23 = 0
+let InsertLocation22 = 0
+let WordIndice22 = 0
+let response22 = 0
+let WordIndice2 = 0
+let InsertLocation2 = 0
+let response2 = 0
+let question2 = ""
 scene.setBackgroundColor(6)
-tiles.setCurrentTilemap(tilemap`level1`)
+WordList = [["abate", "to stop"], ["abyss", "pit"], ["exiate", "idk tbh"], ["import", "bring in"]]
+tiles.setCurrentTilemap(tilemap`
+    level1
+`)
 encounter = 0
 P1 = sprites.create(img`
-    . . . . f f f f . . . . . 
-    . . f f f f f f f f . . . 
-    . f f f f f f c f f f . . 
-    f f f f f f c c f f f c . 
-    f f f c f f f f f f f c . 
-    c c c f f f e e f f c c . 
-    f f f f f e e f f c c f . 
-    f f f b f e e f b f f f . 
-    . f 4 1 f 4 4 f 1 4 f . . 
-    . f e 4 4 4 4 4 4 e f . . 
-    . f f f e e e e f f f . . 
-    f e f b 7 7 7 7 b f e f . 
-    e 4 f 7 7 7 7 7 7 f 4 e . 
-    e e f 6 6 6 6 6 6 f e e . 
-    . . . f f f f f f . . . . 
-    . . . f f . . f f . . . . 
+        . . . . f f f f . . . . . 
+            . . f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f f f c c f f f c . 
+            f f f c f f f f f f f c . 
+            c c c f f f e e f f c c . 
+            f f f f f e e f f c c f . 
+            f f f b f e e f b f f f . 
+            . f 4 1 f 4 4 f 1 4 f . . 
+            . f e 4 4 4 4 4 4 e f . . 
+            . f f f e e e e f f f . . 
+            f e f b 7 7 7 7 b f e f . 
+            e 4 f 7 7 7 7 7 7 f 4 e . 
+            e e f 6 6 6 6 6 6 f e e . 
+            . . . f f f f f f . . . . 
+            . . . f f . . f f . . . .
     `, SpriteKind.Player)
-P1.setPosition(0, 118)
-P1.setVelocity(50, 50)
+let coin = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 5 5 5 . . . . 
+            . . . . 5 5 5 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+            . . . . 5 5 5 5 5 5 5 5 5 . . . 
+            . . . . . 5 5 5 5 5 5 5 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    `, SpriteKind.Food)
+coin.setPosition(30, 105)
+P1.setPosition(10, 100)
+P1.setVelocity(20, 60)
 scene.cameraFollowSprite(P1)
+forever(function on_forever() {
+    if (P1.isHittingTile(CollisionDirection.Right)) {
+        P1.vy = 0
+    } else {
+        P1.vy = 10
+    }
+    
+    if (P1.isHittingTile(CollisionDirection.Bottom)) {
+        P1.vy = 0
+    } else {
+        P1.vy = 10
+    }
+    
+})
